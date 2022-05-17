aacf4537e0c014e5f108718557b0f1c0da1e7f46
Jonathan Sharman <jsharman@niche.com>
2022-05-17 11:54:44 -0500

    Generate a simple image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use image::{ImageBuffer, Rgb, RgbImage};
+
+const WIDTH: u32 = 100;
+const HEIGHT: u32 = 100;
+const FILENAME: &str = "out/stripes.png";
+
 fn main() {
-	println!("Hello, world!");
+	let mut image: RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
+	for x in 0..WIDTH {
+		for y in 0..HEIGHT {
+			let pixel = if (x + y) / 10 % 2 == 0 {
+				Rgb([0, 0, 0])
+			} else {
+				Rgb([255, 255, 255])
+			};
+			image.put_pixel(x, y, pixel);
+		}
+	}
+	match image.save(FILENAME) {
+		Ok(_) => println!("Saved image to {FILENAME}"),
+		Err(err) => println!("Error saving image to {FILENAME}: {err}"),
+	}
 }
